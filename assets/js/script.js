9ce46f919fea5c652bd5271a6f665db196d183ff
Connor Ashlock <connor9804@yahoo.com>
2020-07-29 18:21:51 -0700

    Completed user can flip cards feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,41 @@
-var cardSec = document.getElementById('card-sec');
+var gameCards = document.getElementById('gameCards');
 var statsSec = document.getElementById('stats-sec');
-var cardSecChildren = cardSec.children;
+var cardSecChildren = gameCards.children;
 
+var picArray = [
+  "js-logo",
+  "css-logo",
+  "docker-logo",
+  "github-logo",
+  "html-logo",
+  "mysql-logo",
+  "node-logo",
+  "php-logo",
+  "react-logo"
+]
+
+var addImage = 0;
 
 for (var i = 0; i < cardSecChildren.length; i++){
   var cardBack = document.createElement('div');
-  cardBack.className = 'card-back'
+  cardBack.className = 'card-back';
   var cardFront = document.createElement('div');
   cardFront.className = 'card-front';
-  cardSecChildren[i].append(cardBack, cardFront);
+  cardFront.classList.add(picArray[addImage]);
+  cardSecChildren[i].append(cardFront, cardBack);
   cardSecChildren[i].classList.add('card');
+  addImage++;
+  if (addImage === 9) {
+    addImage = 0;
+  }
+}
+
+gameCards.addEventListener('click', handleClick);
+
+function handleClick(event){
+  if (event.target.className.indexOf("card-back") === -1) {
+    return;
+  }
+  console.log(event);
+  event.target.classList.add('hidden');
 }
